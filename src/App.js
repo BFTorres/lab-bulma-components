6e6115bcd09847ac19b4dcf62ed1239376eb2d28
BFTorres <bfonseca@gmx.de>
2021-05-04 01:35:05 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import "bulma/css/bulma.css";
+import "./App.css";
+import Navbar from "./navbar/Navbar";
+import Signup from "./signup/Signup"
+import FormField from './formfield/FormField'
+import CoolButton from './coolButton/CoolButton'
+
 
 const App = () => {
-  return <h1>Bulma</h1>;
-};
+  
+
+  return <div> <Navbar />
+  <FormField label = {"Name"} placeholder = {'Alex Smith'} type= {'text'}/>
+  <FormField label = {"Email"} placeholder = {'e.g. alexsmith@gmail.com'} type= {'email'}/>
+  <CoolButton   isDanger >Button 1</CoolButton>
+  <CoolButton isInfo >Button 2</CoolButton>
+
+
+  </div>
+
+}
 
 export default App;
